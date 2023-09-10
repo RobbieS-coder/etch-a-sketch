296cbd350224399fa31266978c43ebaa62552016
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-09-10 07:28:25 +0100

    Add change grid size button
    Change the grid size to what the user enters in a prompt. However, a bug is that the nth time the button is pressed, a prompt shows up 2^n-1 times.

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-function initialiseGrid() {
+function initialiseGrid(gridDimensions) {
 	const gridContainer = document.querySelector(".grid-container");
 
-	for (let i = 0; i < 16; i++) {
+	for (let i = 0; i < gridDimensions; i++) {
 		const row = document.createElement("div");
 		row.classList.add("row");
 
-		for (let j = 0; j < 16; j++) {
+		for (let j = 0; j < gridDimensions; j++) {
 			const tile = document.createElement("div");
 			tile.classList.add("tile");
 			row.appendChild(tile);
 			tile.addEventListener("mouseenter", (e) => {
 				e.target.classList.add("hovered");
-			})
+			});
 		}
 
 		gridContainer.appendChild(row);
 	}
+
+	addButtonEventListeners();
 }
 
-initialiseGrid();
+function addButtonEventListeners () {
+	gridSizeButton = document.querySelector(".grid-size");
+
+	gridSizeButton.addEventListener("click", () => {
+		changeGridSize();
+	})
+}
+
+function changeGridSize() {
+	const newDimension = prompt("What do you want the new grid size to be?");
+
+	const rows = document.querySelectorAll(".row");
+	rows.forEach(row => {
+		row.remove();
+	});
+	
+	initialiseGrid(newDimension);
+}
+
+initialiseGrid(16);
